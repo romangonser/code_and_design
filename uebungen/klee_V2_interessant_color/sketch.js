ea8c84cc08a26d2778da42fb761734409b07ae4b
romangonser <roman.gonser@stud.sfgz.ch>
2025-10-27 18:04:10 +0100

    version_monday_1

--- uebungen/klee_V2_interessant_color/sketch.js
+++ uebungen/klee_V2_interessant_color/sketch.js
@@ -6,42 +6,49 @@ let horizontalstroke;
 let winkel;
 
 
-
 function setup() {
   createCanvas(800, 800);
 
-  durchmesser = createSlider(20, 500, 100)
+  angleMode(DEGREES);
+
+  durchmesser = createSlider(20, 500, 200)
   durchmesser.position(20, 10);
 
-  verticalstroke = createSlider(0, 100, 10)
+  verticalstroke = createSlider(0, 400, 200)
   verticalstroke.position(230, 10);
 
   horizontalstroke = createSlider(0, 100, 10)
   horizontalstroke.position(430, 10);
 
-  winkel = createSlider(0, TWO_PI, 0.1, 0)
+  winkel = createSlider(-45, 45, 0)
   winkel.position(600, 10);
   
-
+ 
 
 }
 
 
 function draw() {
-   background(200, 220, 255,);
+   //background(200, 220, 255,);
+background(0, 0, 0,);
 
   let d = durchmesser.value();
   let rund = d;
   let Vstroke = verticalstroke.value();
-  let Hstroke = horizontalstroke.value();
-  let angle = winkel.value();
-
-
+  let Hstroke = 400 -  verticalstroke.value();
+  //let Hstroke = horizontalstroke.value();
+  let drehen = winkel.value();
+    
+push(); 
   fill(255, 0, 0,100)
   noStroke()
   rectMode(CENTER)
+  rotate(drehen);
+
   rect(200, 200, d, d, rund);
+  
   rect(200, 400, d, d);
+     pop();
   rect(200, 600, d, d, rund);
   rect(400, 200, d, d);
   rect(400, 400, d, d, rund);
@@ -49,12 +56,17 @@ function draw() {
   rect(600, 200, d, d, rund);
   rect(600, 400, d, d);
   rect(600, 600, d, d, rund);
+ 
 
-  stroke(255, 0, 0, 20);
+
+  rotate(0);
+  stroke(255, 255, 0, 50);
   strokeWeight(Vstroke)
   line(200, 0, 200, 800);
   line(400, 0, 400, 800);
   line(600, 0, 600, 800);
+
+    stroke(255, 0, 255, 50);
   strokeWeight(Hstroke)
   line(0, 200, 800, 200);
   line(0, 400, 800, 400);
