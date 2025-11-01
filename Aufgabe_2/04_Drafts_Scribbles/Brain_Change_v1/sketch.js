faa970eb93e4535815034646201db7c0fe2dd8d0
romangonser <roman.gonser@stud.sfgz.ch>
2025-11-01 22:16:23 +0100

    brain-change

--- Aufgabe_2/04_Drafts_Scribbles/Brain_Change_v1/sketch.js
+++ Aufgabe_2/04_Drafts_Scribbles/Brain_Change_v1/sketch.js
@@ -3,6 +3,7 @@ let startX, startY;
 let maxRadius = 50;
 let wirt;
 let punkt1, punkt2;
+let newColor;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,42 +12,64 @@ function setup() {
   startY = random(height);
   posX = startX;
   posY = startY;
-  
+
   // Punkte für Linie bestimmen
   punkt1 = createVector(posX, posY);
   punkt2 = createVector(posX, posY);
+
 }
 
 function draw() {
+
+
   // ohne click Schlaufe
   posX += random(-10, 10);
   posY += random(-10, 10);
   wirt = createVector(posX, posY);
   wirt.add(p5.Vector.random2D().mult(1));
+
+   //test, ob an der zielposition wirt.x, wirt.y bereits Farbe liegt und welche
+  let c = get(wirt.x, wirt.y);
+  let transparenz = alpha(c);
+  //console.log(rotkanal, blaukanal, gruenkanal)
+
+
+if(transparenz > 255/2){
+  // es hat hier bereits farbe
   
-  fill(100, 50);
+  for(let i = 0; i < 5; i++){
+    newColor = color(random(255), random(255), random(255), 50);
+  }
+   fill(newColor);
+} else {
+  fill(200, 0, 100, 50);
+}
+
+
+
+
 
   // Distanz vom aktuellen Nullpunkt berechnen
   let distanceFromStart = dist(wirt.x, wirt.y, startX, startY);
-  
+
   // Wenn Radius größer als maxRadius, neuen Wirt erstellen
   if (distanceFromStart > maxRadius) {
     punkt1 = punkt2.copy();
     punkt2 = createVector(posX, posY);
-    
-   // Linie zeichnen
-    stroke(0, 50);
-    strokeWeight(2);
-    line(punkt1.x, punkt1.y, punkt2.x, punkt2.y);
+
 
     // Neuen Wirt erstellen (wenn dieser Teil vor Linie Zeichnen ist, Linie wird zuerst gezeichnet)
     startX = random(width);
     startY = random(height);
     posX = startX;
     posY = startY;
-  
 
-    
+    // Linie zeichnen
+    stroke(0, 50);
+    strokeWeight(2);
+    line(punkt1.x, punkt1.y, punkt2.x, punkt2.y);
+
+
   }
 
   // Wirt zeichnen
@@ -59,11 +82,11 @@ function mousePressed() {
   // Linie vom letzten Punkt zur Mausposition zeichnen
   punkt1 = punkt2.copy();
   punkt2 = createVector(mouseX, mouseY);
-  
+
   stroke(0, 50);
   strokeWeight(2);
   line(punkt1.x, punkt1.y, punkt2.x, punkt2.y);
-  
+
   // Neuen Wirt an Mausposition erstellen
   startX = mouseX;
   startY = mouseY;
